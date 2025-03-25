3075394f9a06dd4b4bbb9f6916dece588f9eb57f
AerrowV <businessasimk@gmail.com>
2025-03-25 11:18:09 +0100

    Added DAO methods to gun and map

--- SP2/src/main/java/dat/dao/impl/GunDAO.java
+++ SP2/src/main/java/dat/dao/impl/GunDAO.java
@@ -1,5 +1,93 @@
 package dat.dao.impl;
 
-public class GunDAO
-{
+import dat.dao.IDAO;
+import dat.entities.Gun;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Random;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class GunDAO implements IDAO<Gun, Long> {
+
+    private static GunDAO instance;
+    private static EntityManagerFactory emf;
+
+    public static GunDAO getInstance(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new GunDAO();
+        }
+        return instance;
+    }
+
+    @Override
+    public Gun read(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.find(Gun.class, id);
+        }
+    }
+
+    @Override
+    public List<Gun> readAll() {
+        try (EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Gun> query = em.createQuery("SELECT g FROM Gun g", Gun.class);
+            return query.getResultList();
+        }
+    }
+
+    @Override
+    public Gun create(Gun gun) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(gun);
+            em.getTransaction().commit();
+            return gun;
+        }
+    }
+
+    @Override
+    public Gun update(Long id, Gun updatedData) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Gun existing = em.find(Gun.class, id);
+            if (existing == null) {
+                em.getTransaction().rollback();
+                return null;
+            }
+
+            existing.setName(updatedData.getName());
+            existing.setGame(updatedData.getGame());
+
+            em.getTransaction().commit();
+            return existing;
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Gun gun = em.find(Gun.class, id);
+            if (gun != null) {
+                em.remove(gun);
+            }
+            em.getTransaction().commit();
+        }
+    }
+
+    public Gun getRandomByGameId(Long gameId) {
+        try (EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Gun> query = em.createQuery("SELECT g FROM Gun g WHERE g.game.id = :gameId", Gun.class);
+            query.setParameter("gameId", gameId);
+
+            List<Gun> guns = query.getResultList();
+            if (guns.isEmpty()) return null;
+
+            return guns.get(new Random().nextInt(guns.size()));
+        }
+    }
 }
--- SP2/src/main/java/dat/dao/impl/MapDAO.java
+++ SP2/src/main/java/dat/dao/impl/MapDAO.java
@@ -1,5 +1,81 @@
 package dat.dao.impl;
 
-public class MapDAO {
+import dat.dao.IDAO;
+import dat.entities.Map;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class MapDAO implements IDAO<Map, Long> {
+
+    private static MapDAO instance;
+    private static EntityManagerFactory emf;
+
+    public static MapDAO getInstance(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new MapDAO();
+        }
+        return instance;
+    }
+
+    @Override
+    public Map read(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.find(Map.class, id);
+        }
+    }
+
+    @Override
+    public List<Map> readAll() {
+        try (EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Map> query = em.createQuery("SELECT m FROM Map m", Map.class);
+            return query.getResultList();
+        }
+    }
+
+    @Override
+    public Map create(Map map) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(map);
+            em.getTransaction().commit();
+            return map;
+        }
+    }
+
+    @Override
+    public Map update(Long id, Map updatedData) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Map existing = em.find(Map.class, id);
+            if (existing == null) {
+                em.getTransaction().rollback();
+                return null;
+            }
+
+            existing.setName(updatedData.getName());
+            existing.setGame(updatedData.getGame());
+            existing.setStrategies(updatedData.getStrategies());
+
+            em.getTransaction().commit();
+            return existing;
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Map map = em.find(Map.class, id);
+            if (map != null) {
+                em.remove(map);
+            }
+            em.getTransaction().commit();
+        }
+    }
 }
